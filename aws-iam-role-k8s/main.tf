a5d8a3e1ac9bab44c3130c8a47b0b928b6d21f0c
Reuben Mannell <reuben@reubit.com>
2022-10-07 20:45:49 +1100

    [aws-iam-role-k8s] Update default iam role name pattern

--- aws-iam-role-k8s/main.tf
+++ aws-iam-role-k8s/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "role" {
-  name               = local.name
+  name               = local.role_name
   path               = var.path
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
   tags               = local.aws_tags

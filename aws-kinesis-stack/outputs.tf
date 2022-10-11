acee1b2a74c57860118ed4ece57ad46f02184f95
Reuben Mannell <reuben@reubit.com>
2022-10-11 16:51:03 +1100

    [aws-kinesis-stack] Fix outputs

--- aws-kinesis-stack/outputs.tf
+++ aws-kinesis-stack/outputs.tf
@@ -3,9 +3,9 @@ output "kinesis_stream_name" {
 }
 
 output "kinesis_firehose_stream_name" {
-  value = aws_kinesis_firehose_delivery_stream.twitter_source[0].name
+  value = local.app_environment == local.app_static_environment ? aws_kinesis_firehose_delivery_stream.twitter_source[0].name : ""
 }
 
 output "kinesis_firehose_iam_role_name" {
-  value = aws_iam_role.twitter_source_firehose[0].name
+  value = local.app_environment == local.app_static_environment ? aws_iam_role.twitter_source_firehose[0].name : ""
 }
